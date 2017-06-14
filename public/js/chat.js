eecfc746b4f438baf2fdc2d7787a2a9dd9773641
Lucero07 <ing.lucero.ramos@gmail.com>
2017-06-13 23:45:30 -0500

    chat funcionalidad

---
+++ public/js/chat.js
@@ -0,0 +1,19 @@
+
+//
+// $.ajax("http://api.tronalddump.io/random/meme", {
+//   method: "GET",
+//   dataType: "jsonp",
+//   // headers: {
+//   //   Accept: "image/jpeg; charset=utf-8",
+//   //   "Content-Type": "image/jpeg; charset=utf-8"
+//   // },
+//   beforeSend: function(xhrObj){
+//           xhrObj.setRequestHeader("Content-Type","image/jpeg");
+//           xhrObj.setRequestHeader("Accept","image/jpeg");
+//   },
+//   success: function (elemento) {
+//   console.log(elemento);
+// }, error: function (error) {
+//   console.log(error);
+// }
+// });
